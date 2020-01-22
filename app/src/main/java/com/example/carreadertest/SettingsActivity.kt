664632181e16179fd3651009d529a4b98b5bd79a
Beke András <beke.andras.mkolc@gmail.com>
2020-01-22 16:23:22 +0100

    Commit 0

--- app/src/main/java/com/example/carreadertest/SettingsActivity.kt
+++ app/src/main/java/com/example/carreadertest/SettingsActivity.kt
@@ -3,6 +3,10 @@ package com.example.carreadertest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import android.view.View
+import android.util.Log
+import java.io.File
+import android.content.Intent
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -21,4 +25,45 @@ class SettingsActivity : AppCompatActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
     }
+    /** Called when the user touches the directory select button */
+    fun selectdirectory(view: View) {
+
+
+        val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        i.addCategory(Intent.CATEGORY_DEFAULT)
+        startActivityForResult(Intent.createChooser(i, "File Chooser"), 9999)
+     //   val chooserIntent = Intent.createChooser(i, "File Chooser")
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val fileName = "data.txt"
+        if (requestCode == 9999) {
+
+            val uri = data!!.getData()
+            Log.i("Test", "RAW URI " + uri)
+            val pathName = uri!!.getPath()
+            Log.i("Test", "PathName " + pathName)
+  //          val v = pathName + '/' + fileName
+            val v = "test.txt"
+            Log.i("Test", "Get total filename " + v)
+
+            val file = File(v)
+
+             // create a new file
+                val isNewFileCreated: Boolean = file.createNewFile()
+
+                if (isNewFileCreated) {
+                    println("$fileName is created successfully.")
+                } else {
+                    println("$fileName already exists.")
+                }
+
+        }
+        when (requestCode) {
+            9999 -> Log.i("Test", "Result URI " + data!!.data!!)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
